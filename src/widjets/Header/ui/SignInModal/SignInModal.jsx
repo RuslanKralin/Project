063d8e5d55a7307dd9e9f56a5ad99d7ee43e8b4a
kralin.front <kralin.front@gmail.com>
2023-01-15 02:19:12 +0300

    feat(FE-4): add formic in sign in modal

--- src/widjets/Header/ui/SignInModal/SignInModal.jsx
+++ src/widjets/Header/ui/SignInModal/SignInModal.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Box, Button, Modal, TextField } from '@mui/material'
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd'
+import { Formik } from 'formik';
+import { display } from '@mui/system';
 // import Button from '@mui/material/Button';
 // import Typography from '@mui/material/Typography';
 // import Modal from '@mui/material/Modal';
@@ -10,7 +12,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    minWidth: 400,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -22,9 +24,9 @@ const style = {
 
 function SignInModal() {
     const [open, setOpen] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, SetPassword] = useState('');
-    const [confirm, SetConfirm] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, SetPassword] = useState('');
+    // const [confirm, SetConfirm] = useState('');
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
@@ -33,7 +35,7 @@ function SignInModal() {
             <Button onClick={handleOpen} variant="outlined" startIcon={<AssignmentIndIcon />}>
                 Sign in
             </Button>
-            
+
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -41,7 +43,83 @@ function SignInModal() {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <TextField
+                    <Formik
+                        initialValues={{ email: '', password: '', confirm: '' }}
+                        validate={values => {
+                            const errors = {};
+                            if (values.email.length < 3) {
+                                errors.email = 'Less then 3';
+                                // } else if (
+                                //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                // ) {
+                                //     errors.email = 'Invalid email address';
+                            } else if (values.email.length > 30) {
+                                errors.email = 'More then 30!!!'
+                            } else if (values.password !=values.confirm){
+                                errors.confirm  = 'Check your password please'
+                            }
+                            return errors;
+                        }}
+                        onSubmit={(values) => {
+                            console.log(values)
+
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting,
+                            /* and other goodies */
+                        }) => (
+                            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+                                <TextField
+                                    type="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.email}
+                                    label="Email"
+                                />
+
+                                {errors.email && touched.email && errors.email}
+                                <TextField
+                                    type="password"
+                                    name="password"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.password}
+                                    label="Password"
+                                />
+
+                                <TextField
+                                    type="password"
+                                    name="confirm"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.confirm}
+                                    label="Confirm"
+                                />
+                               {errors.confirm && touched.confirm && errors.confirm}
+                                <Button type="submit" variant="contained" color="success" disabled={isSubmitting}>
+                                    Submit
+                                    
+                                </Button>
+                            </form>
+                        )}
+                    </Formik>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
+
+export default SignInModal
+
+{/* <TextField
                         required
                         id="outlined-required"
                         label="Email"
@@ -62,11 +140,4 @@ function SignInModal() {
 
                     <Button variant="contained" color="success">
                         SIGN IN
-                    </Button>
-                 </Box>
-            </Modal>
-        </div>
-    );
-}
-
-export default SignInModal
+                    </Button> */}
